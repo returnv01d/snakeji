2bfae1c071da17a2ad0e4e9b611fdb4e495aa9ad
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-04-25 11:04:14 +0200

    refactorized menu initialize method, and fixed config paths

--- lib/snakeji/menu.rb
+++ lib/snakeji/menu.rb
@@ -3,15 +3,20 @@ require_relative 'models/game_model'
 
 class Menu
   def initialize
-    @window = Window.new(height:GameModel.instance.get(:WINDOW_HEIGHT), width:GameModel.instance.get(:WINDOW_WIDTH) )
-    dark_grey = Color.new("#8B7D6B")
-    puts Color::is_hex?("#8B7D6B")
-    @window.set(background: "#1f1f21")
-    @width_padding = GameModel.instance.get(:WINDOW_WIDTH) * (1.0/12)
-    @height_padding = GameModel.instance.get(:WINDOW_HEIGHT)* (1.0/16)
-    @mainpanel = MainPanel.new(0 + @width_padding, 0 + @height_padding,
-                               GameModel.instance.get(:WINDOW_WIDTH) -  @width_padding, GameModel.instance.get(:WINDOW_HEIGHT) * (13.0/16) - @height_padding)
-    @window.add(@mainpanel.rects)
+    @width = GameModel.instance.get(:WINDOW_WIDTH)
+    @height = GameModel.instance.get(:WINDOW_HEIGHT)
+    @window = Window.new(height: @height, width: @width)
+    @window.set(background: '#1f1f21')
+    @width_padding = @width * (1.0 / 16)
+    @height_padding = @height * (1.0 / 12)
+    create_view
+  end
+
+  def create_view
+    @main_panel = MainPanel.new(0 + @width_padding, 0 + @height_padding,
+                                @width - @width_padding,
+                                @height * (5 / 6) - @height_padding)
+    @window.add(@main_panel.rects)
     @window.show
   end
 end
@@ -22,18 +27,17 @@ class MainPanel
     @y = y
     @width = width
     @height = height
-    @width_padding =  1.0/10 * @width
-    @height_padding =  1.0/12 * @height
-    @panels = Array.new
-    @rects = Array.new
+    @width_padding = 1.0 / 14 * @width
+    @height_padding = 1.0 / 12 * @height
+    @panels = []
+    @rects = []
+    create_panels
+  end
+
+  def create_panels
     panel_1 = PlayerPanel.new(@x,  @y, @width/2.0 - @width_padding, @height/2.0 - @height_padding )
-    panel_2 = PlayerPanel.new(@width/2.0 + @width_padding,  @y, @width/2.0 - @width_padding, @height/2.0 - @height_padding )
-    panel_3 = PlayerPanel.new(@x,  @y + @height/2 + @height_padding, @width/2.0 - @width_padding, @height/2.0 - @height_padding )
-    panel_4 = PlayerPanel.new(@width/2.0 + @width_padding,  @y + @height/2 + @height_padding, @width/2.0 - @width_padding, @height/2.0 - @height_padding )
-    @panels << panel_1 << panel_2
-    @rects << panel_1.rect << panel_2.rect << panel_3.rect << panel_4.rect
-
-
+    @panels << panel_1
+    @rects << panel_1.rect
   end
 end
 class PlayerPanel
