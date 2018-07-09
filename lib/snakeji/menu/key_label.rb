369cf815b047d0df9405c75a9eb7eaeb7d539625
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-07-09 20:56:11 +0200

    Added observable module, notifies when user selects it.

--- lib/snakeji/menu/key_label.rb
+++ lib/snakeji/menu/key_label.rb
@@ -1,58 +1,87 @@
 require_relative '../../../lib/snakeji/utility/utility'
+require_relative '../../../lib/snakeji/menu/button'
+require_relative 'bounding_box'
 class KeyLabel
+  include Observable
+  attr_accessor :selected
   def initialize(bounding_box, label_content, key_content, rect_color)
     @bounding_box = bounding_box
     @label_content = label_content
     @key_content = key_content
     @rect_color = rect_color
-    create_label_text
-    create_key_text
+    @border_color = rect_color
+    @border_width = GameModel.model['MENU']['KEY_LABEL_BORDER_WIDTH']
+    @selected = false
+    @active = true
+    create_label
+    create_key
+    on_click
+    on_key
   end
 
-  def create_label_text
-    create_label_rect
-    @label_text = create_text(@label_rect.x  + @label_rect.width / 2.0,
-                              @bounding_box.top_left.y + (@bounding_box.height / 2),
-                              @label_content, 15)
+  def active
+    @active = !@active
   end
 
-  def create_label_rect
+  def create_label
+    @label = BorderedButton.new(create_label_bounding_box,
+                                text: @label_content,
+                                border_width: 2,
+                                border_color: @rect_color,
+                                selected_border_color: '#F9EC18',
+                                button_color: @rect_color)
+
+  end
+
+  def create_label_bounding_box
     label_width = @bounding_box.width * GameModel.get_ratio('KEY_LABEL_TEXT_WIDTH')
-    @label_rect = Rectangle.new(x: @bounding_box.top_left.x,
-                                y: @bounding_box.top_left.y,
-                                width: label_width,
-                                height: @bounding_box.height,
-                                color: @rect_color)
-    @label_rect.add
+    BoundingBox.new(@bounding_box.top_left, label_width, @bounding_box.height)
   end
 
-  def create_key_text
-    create_key_rect
-    @key_text = create_text(@key_rect.x + @key_rect.width / 2.0,
-                            @bounding_box.top_left.y + (@bounding_box.height / 2),
-                            @key_content, 15)
-  end
-
-  def create_key_rect
+  def create_key_bounding_box
     key_label_x = @bounding_box.top_left.x + @bounding_box.width * GameModel.get_ratio('KEY_LABEL_KEY_X')
-    @key_rect = Rectangle.new(
-      x: key_label_x,
-      y: @bounding_box.top_left.y,
-      width: @bounding_box.width * 2.0 / 7.0,
-      height: @bounding_box.height,
-      color: @rect_color
+    BoundingBox.new(Point.new(key_label_x, @bounding_box.top_left.y),
+                    @bounding_box.width * 2.0 / 7.0,
+                    @bounding_box.height
     )
-    @key_rect.add
   end
 
-  def create_text(x, y, content, text_size)
-    text = Text.new(x: x,
-                    y: y,
-                    text: content,
-                    font: '../../fonts/Vera.ttf',
-                    size: text_size)
-    Utility.center_text(text)
-    text.add
-    text
+  def create_key
+    @key = BorderedButton.new(create_key_bounding_box,
+                              text: @key_content,
+                              border_width: 2,
+                              border_color: @rect_color,
+                              selected_border_color: '#F9EC18',
+                              button_color: @rect_color)
   end
+
+  def unselect
+    return false unless @selected
+    @selected = false
+    @key.change_border
+    @label.change_border
+  end
+
+  def on_key
+    Application.on :key_down do |e|
+      if @selected && @active
+        @key.text.text = e.key
+      end
+    end
+  end
+
+  def on_click
+    Application.on :mouse_down do |e|
+      if e.button == :left
+        if @bounding_box.contains?(Point.new(e.x, e.y)) && @active
+          @selected = !@selected
+          @key.change_border
+          @label.change_border
+          changed(true)
+          notify_observers(self)
+        end
+      end
+    end
+  end
+
 end
