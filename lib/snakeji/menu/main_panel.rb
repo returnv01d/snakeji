ffbb06b955529e195994feb509dbac62d3488d14
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-08 11:19:14 +0200

    initial MainPanel and KeyPanel redesign

--- lib/snakeji/menu/main_panel.rb
+++ lib/snakeji/menu/main_panel.rb
@@ -1,42 +1,27 @@
 require_relative 'panel'
 require_relative 'key_panel'
 
-class MainPanel < Panel
-  def initialize(bounding_box, color)
-    super(bounding_box, color)
-    @width_padding = GameModel.get_ratio('MAIN_PANEL_WIDTH_PADDING') * @bounding_box.width
-    @height_padding = GameModel.get_ratio('MAIN_PANEL_HEIGHT_PADDING') * @bounding_box.height
+class MainPanel < CompositeUIElement
+
+  WIDTH = GameModel.model['WINDOW_WIDTH']
+  HEIGHT = GameModel.model['WINDOW_HEIGHT'] * 5.0 / 6.0
+  BG_COLOR = GameModel.model['MENU']['BG_COLOR']
+
+  def initialize()
+    super(WIDTH, HEIGHT, bg_color: BG_COLOR)
+    @width_padding = GameModel.get_ratio('MAIN_PANEL_WIDTH_PADDING') * WIDTH
+    @height_padding = GameModel.get_ratio('MAIN_PANEL_HEIGHT_PADDING') * HEIGHT
     @player_panel_color = GameModel.model['MENU']['PLAYER_PANEL_COLOR']
     @key_labels = []
-    create_panels
+    #create_panels
     observe
   end
 
-  def create_panels
-    2.times do |height_step|
-      2.times do |width_step|
-        create_panel(create_panel_point(height_step, width_step), true)
-      end
-    end
-  end
-
-  def create_panel_point(height_step, width_step)
-    Point.new(
-        @bounding_box.top_left.x + width_step * (@bounding_box.width / 2.0) + @width_padding,
-        @bounding_box.top_left.y + height_step * (@bounding_box.height / 2.0 + @height_padding)
-    )
-  end
-
-  def create_panel(offset_point, active)
-    bounding_box = BoundingBox.new(
-        offset_point,
-        @bounding_box.width / 2.0 - 2 * @width_padding,
-        @bounding_box.height / 2.0 - @height_padding
-    )
-    panel = KeyPanel.new(bounding_box,
-                         active: active,
-                         active_color: @player_panel_color
-    )
+  def create_panel(width_step, height_step)
+    panel = KeyPanel.new(parent: self)
+    panel_x = @x + width_step * (@width / 2.0) + @width_padding
+    panel_y = @y + height_step * (@height / 2.0) + @height_padding
+    panel.draw(panel_x, panel_y)
     @key_labels += panel.key_labels
     panel.on_click
   end
@@ -49,6 +34,14 @@ class MainPanel < Panel
     @key_labels -= [key_label]
     @key_labels.each(&:unselect)
     @key_labels << key_label
-
   end
+
+  def draw(top_left_x, top_left_y)
+    super(top_left_x, top_left_y)
+    2.times do |height_step|
+      2.times do |width_step|
+        create_panel(height_step, width_step)
+      end
+    end
+    end
 end
