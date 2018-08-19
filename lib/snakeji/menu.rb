3561e5f57d0a78429a6888cd5609e6ff7200eb26
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-20 00:05:42 +0200

    Cleaned old stuff, added draw method

--- lib/snakeji/menu.rb
+++ lib/snakeji/menu.rb
@@ -1,50 +1,41 @@
 require 'ruby2d'
-require 'ruby2d/dsl'
-require_relative 'menu/bounding_box'
 require_relative 'models/game_model'
-require_relative 'utility/point'
 require_relative 'menu/button_panel'
 require_relative 'menu/main_panel'
-require_relative 'menu/Base/ui_element'
-require_relative 'menu/Decorators/border_decoration'
 require_relative 'menu/bottom_button'
 class Menu
+
+  attr_accessor :height_padding
   def initialize
     @width = GameModel.model['WINDOW_WIDTH']
     @height = GameModel.model['WINDOW_HEIGHT']
     @bg_color = GameModel.model['MENU']['BG_COLOR']
     Application.set(height: @height, width: @width, background: @bg_color)
-    initialize_properties
-    create_view
-  end
-
-  def initialize_properties
     @width_padding = @width * 1.0 / 32.0
     @height_padding = @height * 1.0 / 24.0
-    @main_panel_height = @height * GameModel.get_ratio('MAIN_PANEL_HEIGHT')
+    create_view
+
   end
 
   def create_view
-   # create_button_panel
+    #create_button_panel
     create_main_panel
-    thiss = BottomButton.new()
-    thiss.draw(100, 100)
-    puts thiss.class
+    draw(0, 0)
     Application.show
   end
 
   def create_main_panel
-    point = Point.new(0, @height_padding)
-    bounding_box = BoundingBox.new(point, @width, @main_panel_height - 2 * @height_padding)
-    @main_panel = MainPanel.new()
-    @main_panel.draw(0, @height_padding)
+    @main_panel = MainPanel.new(parent: self)
   end
 
   def create_button_panel
-    button_panel_color = GameModel.model['MENU']['BUTTON_PANEL_COLOR']
-    point = Point.new(0, @main_panel_height - @height_padding)
     bounding_box = BoundingBox.new(point, @width, @height - @main_panel_height + @height_padding)
     @button_panel = ButtonPanel.new(bounding_box, button_panel_color)
+  end
+
+  def draw(top_left_x, top_left_y)
+    @main_panel.draw(0, @height_padding)
+    #@button_panel.draw(0, @main_panel.height - @height_padding)
   end
 end
 
