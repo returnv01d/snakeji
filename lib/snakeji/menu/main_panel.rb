05d613a8209c341ea8544d0825d071c86fa89020
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-02 17:44:06 +0200

    Redesign to use draw method with Table Alignment and Composite pattern, change way of creating panels.

--- lib/snakeji/menu/main_panel.rb
+++ lib/snakeji/menu/main_panel.rb
@@ -1,36 +1,33 @@
-require_relative 'panel'
+require_relative '../menu/Alignments/horizontal_alignment'
+require_relative '../menu/Alignments/vertical_alignment'
+require_relative '../menu/Alignments/table_alignment'
 require_relative 'key_panel'
 
 class MainPanel < CompositeUIElement
-
-  WIDTH = GameModel.model['WINDOW_WIDTH']
-  HEIGHT = GameModel.model['WINDOW_HEIGHT'] * 5.0 / 6.0
   BG_COLOR = GameModel.model['MENU']['BG_COLOR']
 
-  def initialize()
-    super(WIDTH, HEIGHT, bg_color: BG_COLOR)
-    @width_padding = GameModel.get_ratio('MAIN_PANEL_WIDTH_PADDING') * WIDTH
-    @height_padding = GameModel.get_ratio('MAIN_PANEL_HEIGHT_PADDING') * HEIGHT
-    @player_panel_color = GameModel.model['MENU']['PLAYER_PANEL_COLOR']
+  def initialize(opts = {})
+    @parent = opts[:parent]
+    @width = GameModel.model['WINDOW_WIDTH']
+    @height = GameModel.model['WINDOW_HEIGHT'] * 5.0 / 6.0 - 2 * @parent.height_padding
+    super(@width, @height, bg_color: BG_COLOR, parent: @parent)
+    @width_padding = 1.0 / 16.0 * @width
+    @height_padding = 1.0 / 18.0 * @height
     @key_labels = []
-    #create_panels
-    observe
   end
 
-  def create_panel(width_step, height_step)
-    panel = KeyPanel.new(parent: self)
-    panel_x = @x + width_step * (@width / 2.0) + @width_padding
-    panel_y = @y + height_step * (@height / 2.0) + @height_padding
-    panel.draw(panel_x, panel_y)
-    @key_labels += panel.key_labels
+  def create_panel(id, active)
+    panel = KeyPanel.new(id, parent: self, active: active)
     panel.on_click
+    @key_labels += panel.sub_elements
+    add_sub_element(panel)
   end
 
   def observe
-    @key_labels.each{|label| label.add_observer(self) }
+    @key_labels.each { |label| label.add_observer(self) }
   end
 
-  def update key_label
+  def update(key_label)
     @key_labels -= [key_label]
     @key_labels.each(&:unselect)
     @key_labels << key_label
@@ -38,10 +35,20 @@ class MainPanel < CompositeUIElement
 
   def draw(top_left_x, top_left_y)
     super(top_left_x, top_left_y)
-    2.times do |height_step|
-      2.times do |width_step|
-        create_panel(height_step, width_step)
-      end
-    end
-    end
+
+    create_panels
+
+    horizontal_spacing = (@width / 2.0)
+    vertical_spacing = @height / 2.0 + @height_padding
+
+    panels_alignment = TableAlignment.new(@sub_elements, 2,
+                                          horizontal_spacing, vertical_spacing)
+    panels_alignment.draw(top_left_x + @width_padding, top_left_y)
+  end
+
+  def create_panels
+    create_panel(1, true); create_panel(2, true)
+    create_panel(3, false); create_panel(4, false)
+    observe
+  end
 end
