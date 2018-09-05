19a193493e335770ed158a22187c21ac86c58513
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-05 19:32:48 +0200

    Add 'underscore' method from Rails for converting ClassNames to symbol_names.

--- lib/snakeji/utility/string.rb
+++ lib/snakeji/utility/string.rb
@@ -4,4 +4,15 @@ class String
     return true if self =~ /\A\d+\Z/
     true if Float(self) rescue false
   end
+
+  # taken from Rails ActiveSupport::Inflector
+  def underscore
+    word = self.dup
+    word.gsub!(/::/, '/')
+    word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.tr!("-", "_")
+    word.downcase!
+    word
+  end
 end
