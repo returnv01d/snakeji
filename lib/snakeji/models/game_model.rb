f0be3c1159fda2f94c098a96e1e8dbf771f43890
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-06 13:18:59 +0200

    Delete ratio handling from GameModel singleton.

--- lib/snakeji/models/game_model.rb
+++ lib/snakeji/models/game_model.rb
@@ -7,16 +7,11 @@ class GameModel
   attr_accessor :model
   def initialize
     @model = Utility::read_config_to_hash(CONFIG_FILE_PATH)
-    @model['MENU']['RATIOS'].each { |_, value| value = value.to_r.to_f }
   end
 
   @@instance = GameModel.new
   def self.model
     @@instance.model
-  end
-
-  def self.get_ratio(name)
-    @@instance.model['MENU']['RATIOS'][name].to_r.to_f
   end
 
   private_class_method :new
