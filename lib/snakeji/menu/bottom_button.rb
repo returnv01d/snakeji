27455eb87da07692d8e52b837a3f6c7a22224cd4
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-07 15:42:13 +0200

    Use constants 'TEXT' and 'BG_COLOR' in initialization.

--- lib/snakeji/menu/bottom_button.rb
+++ lib/snakeji/menu/bottom_button.rb
@@ -1,14 +1,14 @@
 require_relative 'Base/button'
 class BottomButton < Button
 
+  BG_COLOR = GameModel.model['MENU']['BOTTOM_BUTTON_BG_COLOR']
   BORDER_COLOR = GameModel.model['MENU']['BOTTOM_BUTTON_BORDER_COLOR']
+  TEXT = GameModel.model['MENU']['BOTTOM_BUTTON_TEXT']
 
   def initialize
-    @width = GameModel.model['WINDOW_WIDTH'] * (1.0 / 16.0)
-    @height = GameModel.model['WINDOW_HEIGHT'] * (1.0 / 16.0)
-    @bg_color = GameModel.model['MENU']['BOTTOM_BUTTON_COLOR']
-    @text = GameModel.model['MENU']['BOTTOM_BUTTON_TEXT']
-    super(@width, @height, @text, bg_color: @bg_color, border_color: BORDER_COLOR)
+    width = GameModel.model['WINDOW_WIDTH'] * (1.0 / 16.0)
+    height = GameModel.model['WINDOW_HEIGHT'] * (1.0 / 16.0)
+    super(width, height, TEXT, bg_color: BG_COLOR, border_color: BORDER_COLOR)
   end
 
 end
