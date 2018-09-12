6bbbd0185c274271cab0eb36b895931956f2aecb
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-12 20:49:58 +0200

    Change color on mouse hover.Rename Constant to add connotation with bg.Change text-size

--- lib/snakeji/menu/bottom_button.rb
+++ lib/snakeji/menu/bottom_button.rb
@@ -2,13 +2,28 @@ require_relative 'Base/button'
 class BottomButton < Button
 
   BG_COLOR = GameModel.model['MENU']['BOTTOM_BUTTON_BG_COLOR']
-  BORDER_COLOR = GameModel.model['MENU']['BOTTOM_BUTTON_BORDER_COLOR']
+  INACTIVE_BG_COLOR = GameModel.model['MENU']['BOTTOM_BUTTON_INACTIVE_BG_COLOR']
   TEXT = GameModel.model['MENU']['BOTTOM_BUTTON_TEXT']
 
   def initialize
-    width = GameModel.model['WINDOW_WIDTH'] * (1.0 / 16.0)
+    width = GameModel.model['WINDOW_WIDTH'] * (1.0 / 10.0)
     height = GameModel.model['WINDOW_HEIGHT'] * (1.0 / 16.0)
-    super(width, height, TEXT, bg_color: BG_COLOR, border_color: BORDER_COLOR)
+    text_size = 18
+    super(width, height, TEXT, bg_color: BG_COLOR, border_color: BG_COLOR, text_size: text_size)
+    on_mouse_move
+  end
+
+  def on_mouse_move
+    Application.on :mouse_move do |e|
+      if contains?(e.x, e.y)
+        @border.color = BG_COLOR
+        @rect.color = BG_COLOR
+      else
+        @rect.color = INACTIVE_BG_COLOR
+        @border.color = INACTIVE_BG_COLOR
+      end
+
+    end
   end
 
 end
