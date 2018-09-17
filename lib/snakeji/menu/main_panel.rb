5d10077f0158f90c5f09e6aa1f132712aebd135f
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-09-17 22:57:20 +0200

    Activate first emojii in panels after creating and drawing them.Add getter 'active_panels_count' and 'player_keys' methods for communicating with Menu class.

--- lib/snakeji/menu/main_panel.rb
+++ lib/snakeji/menu/main_panel.rb
@@ -21,9 +21,19 @@ class MainPanel < CompositeUIElement
   def create_panel(id, active)
     key_contents = GameModel.model['MENU']['KEYS'][id-1]
     panel = KeyPanel.new(key_contents, parent: self, active: active)
-    panel.add_observer(@emojii_controller, :on_keypanel_change)
     @emojii_controller << panel.emojii_panel
     add_sub_element(panel)
+  end
+
+  def active_panels_count
+    @sub_elements.select(&:active).count
+  end
+
+  def players_keys
+    keys = []
+    active_panels = @sub_elements.select(&:active)
+    active_panels.each { |panel| keys << panel.player_keys }
+    keys
   end
 
   def draw(top_left_x, top_left_y)
@@ -37,11 +47,14 @@ class MainPanel < CompositeUIElement
     panels_alignment = TableAlignment.new(@sub_elements, 2,
                                           horizontal_spacing, vertical_spacing)
     panels_alignment.draw(top_left_x + @width_padding, top_left_y)
+
+    @sub_elements.each(&:activate_first_free_emojii)
   end
 
   def create_panels
     @emojii_controller = EmojiiPanelsController.new
     create_panel(1, true); create_panel(2, true)
     create_panel(3, false); create_panel(4, false)
+
   end
 end
