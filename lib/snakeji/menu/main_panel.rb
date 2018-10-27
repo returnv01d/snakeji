b0cb8d094d104b68a9d3f8f4008abf2184c0dcdf
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-10-27 17:48:02 +0200

    Add 'players_emojiis' method to get selected emojii from panels.Change instance variable 'width' to constant, use separate 'Menu' model.

--- lib/snakeji/menu/main_panel.rb
+++ lib/snakeji/menu/main_panel.rb
@@ -1,25 +1,26 @@
 require_relative '../menu/Alignments/table_alignment'
 require_relative '../menu/Controllers/emojii_panels_controller'
 require_relative 'key_panel'
+require_relative '../menu/Base/composite_ui_element'
 
 class MainPanel < CompositeUIElement
 
   BG_COLOR = GameModel.model['MENU']['BG_COLOR']
+  WIDTH = GameModel.model['MENU']['WINDOW_WIDTH']
 
   attr_accessor :emojii_controller
   def initialize(opts = {})
     parent = opts[:parent]
-    width = GameModel.model['WINDOW_WIDTH']
-    height = GameModel.model['WINDOW_HEIGHT'] * 5.0 / 6.0 - 2 * parent.height_padding
-    width_padding = 1.0 / 16.0 * width
+    height = GameModel.model['MENU']['WINDOW_HEIGHT'] * 5.0 / 6.0 - 2 * parent.height_padding
+    width_padding = 1.0 / 16.0 * WIDTH
     height_padding = 1.0 / 18.0 * height
-    super(width, height,
+    super(WIDTH, height,
           width_padding: width_padding, height_padding: height_padding,
           bg_color: BG_COLOR, parent: parent)
   end
 
   def create_panel(id, active)
-    key_contents = GameModel.model['MENU']['KEYS'][id-1]
+    key_contents = GameModel.model['MENU']['KEYS'][id - 1]
     panel = KeyPanel.new(key_contents, parent: self, active: active)
     @emojii_controller << panel.emojii_panel
     add_sub_element(panel)
@@ -34,6 +35,10 @@ class MainPanel < CompositeUIElement
     active_panels = @sub_elements.select(&:active)
     active_panels.each { |panel| keys << panel.player_keys }
     keys
+  end
+
+  def players_emojiis
+    @emojii_controller.selected_emojiis
   end
 
   def draw(top_left_x, top_left_y)
@@ -55,6 +60,5 @@ class MainPanel < CompositeUIElement
     @emojii_controller = EmojiiPanelsController.new
     create_panel(1, true); create_panel(2, true)
     create_panel(3, false); create_panel(4, false)
-
   end
 end
