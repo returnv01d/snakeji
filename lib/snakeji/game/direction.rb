50bbfd7260213a47bac538f743ec38917dcafc7c
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-11-06 23:03:24 +0100

    Add constant directions.

--- lib/snakeji/game/direction.rb
+++ lib/snakeji/game/direction.rb
@@ -6,4 +6,24 @@ class Direction
     y_velocity =  x_velocity.zero? ? (possible_directions - [0]).sample : 0
     [x_velocity, y_velocity]
   end
+
+  def self.up
+    [0, -1]
+  end
+
+  def self.down
+    [0, 1]
+  end
+
+  def self.is_opposite?(dir1, dir2)
+    ((dir1[0] * -1) == dir2[0]) && ((dir1[1] * -1) == dir2[1])
+  end
+
+  def self.right
+    [1, 0]
+  end
+
+  def self.left
+    [-1, 0]
+  end
 end
